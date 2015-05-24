3f8ddd4935e24b993bea25a229eaa52f3b8839f2
Aurimas Sadauskas <aurimas.sadauskas@visma.com>
2015-05-24 17:42:16 +0300

    NFC Tag reading

---
+++ NFCLogistikaMobile/NFCLogistikaMobile/NFCLogistikaMobile.WindowsPhone/NdefRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NFCLogistikaMobile
+{
+    public class NdefRecord
+    {
+        public bool Mb { get; set; }
+        public bool Me { get; set; }
+        public bool Cf { get; set; }
+        public bool Sr { get; set; }
+        public bool Il { get; set; }
+        public byte Tnf { get; set; }
+
+        public UInt16 TypeLength { get; set; }
+        public UInt16 IdLength { get; set; }
+        public UInt32 PayloadLength { get; set; }
+        public byte[] Type { get; set; }
+        public byte[] Id { get; set; }
+        public byte[] Payload { get; set; }
+
+        // Is Smart Poster record
+        public bool IsSpRecord { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public NdefRecord()
+        {
+            Type = null;
+            Id = null;
+            Payload = null;
+        }
+    }
+}
